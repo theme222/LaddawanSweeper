872d9e0c59c0ad6369d4b698c7bfbc0b116693f4
theme222 <sira.tongsima@yahoo.com>
2024-09-09 21:43:06 +0700

    test1
    asdf

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 # bot.py
 import os
 import discord
+from pybase64 import standard_b64decode as DECODE
 
-# VFZSSk5FMVVVVEpQVkVFeFRXcE5ORTVFUlRSTmFrazBUV2N1UjFaMFN6VkpMamhKUlZZNWFEaGhOWFJrVkdkSFpYbEpVMHA1VG5wT1VuQnVOMVpwYVhwSloyNTJlVVZ6
 # Decode base64 twice
-TOKEN = ''
-
+TOKEN = str(DECODE(DECODE(
+    b'VFZSSk5FMVVVVEpQVkVFeFRXcE5ORTVFUlRSTmFrazBUV2N1UjFaMFN6VkpMamhKUlZZNWFEaGhOWFJrVkdkSFpYbEpVMHA1VG5wT1VuQnVOMVpwYVhwSloyNTJlVVZ6')))[
+        2:-1]
 client = discord.Client(intents=discord.Intents.default())
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run('')
+
+@client.event
+async def on_message(msg):
+    await msg.channel.send("Test")
+
+
+client.run(TOKEN)
